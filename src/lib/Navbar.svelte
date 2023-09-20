2eee16d8d500e3bc70514ddce5da7f53a6b66699
user <samuel_morgan_@outlook.com>
2023-09-19 22:43:45 -0300

    basic structure + navbar + tailwind

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,9 @@
+<!-- Navbar.svelte -->
+<nav class="navbar">
+	<ul class="flex">
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/projects">Projects</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
