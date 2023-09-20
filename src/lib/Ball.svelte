58615d8048ad9f33325415c6a5b7af96e2f6f1dd
user <samuel_morgan_@outlook.com>
2023-09-20 17:34:10 -0300

    noisey gradient ball

---
+++ src/lib/Ball.svelte
@@ -0,0 +1,24 @@
+<div class="w-72 h-72 relative isolate rounded-full overflow-hidden">
+	<div class="noise w-full h-full" />
+	<div class="overlay absolute top-0 w-full h-full" />
+</div>
+
+<style>
+	.noise {
+		background: linear-gradient(20deg, rgb(61, 11, 11), transparent),
+			url(https://grainy-gradients.vercel.app/noise.svg);
+		filter: contrast(170%) brightness(1000%);
+	}
+
+	/* Chrome-specific */
+	@media all and (-webkit-min-device-pixel-ratio: 0) and (min-resolution: 0.001dpcm) {
+		.noise {
+			filter: contrast(290%) brightness(1000%);
+		}
+	}
+
+	.overlay {
+		background: rgb(255, 229, 185);
+		mix-blend-mode: multiply;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Ball from '$lib/Ball.svelte';
+</script>
+
 <h1>Sam Morgan</h1>
 <h2>Fullstack developer</h2>
+<Ball />
