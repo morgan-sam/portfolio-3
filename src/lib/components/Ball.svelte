acc6b6c4e5eb5659200553df4e2b47d5f0206581
user <samuel_morgan_@outlook.com>
2023-09-20 18:48:09 -0300

    basic shadow on lighting on ball

--- src/lib/components/Ball.svelte
+++ src/lib/components/Ball.svelte
@@ -1,23 +1,27 @@
-<div class="w-72 h-72 relative isolate rounded-full overflow-hidden">
-	<div class="noise w-full h-full" />
-	<div class="overlay absolute top-0 w-full h-full" />
+<div class="ball w-72 h-72 relative isolate rounded-full overflow-hidden">
+	<div class="ball-shadow w-full h-full" />
+	<div class="ball-color absolute top-0 w-full h-full" />
 </div>
 
 <style>
-	.noise {
-		background: linear-gradient(20deg, rgb(61, 11, 11), transparent), url('$lib/images/noise.svg');
+	.ball {
+		transform: rotate(200deg);
+	}
+	.ball-shadow {
+		background: radial-gradient(ellipse at 40%, rgba(255, 255, 255, 0), rgb(43, 43, 82)),
+			url('$lib/images/noise.svg');
 		filter: contrast(170%) brightness(1000%);
+		transform: rotate(-150deg);
 	}
 
 	/* Chrome-specific */
 	@media all and (-webkit-min-device-pixel-ratio: 0) and (min-resolution: 0.001dpcm) {
-		.noise {
-			filter: contrast(290%) brightness(1000%);
+		.ball-shadow {
+			filter: contrast(150%) brightness(400%);
 		}
 	}
-
-	.overlay {
-		background: rgb(255, 229, 185);
+	.ball-color {
+		background: radial-gradient(circle at 67% 30%, rgb(255, 229, 185), rgb(24, 5, 8));
 		mix-blend-mode: multiply;
 	}
 </style>
