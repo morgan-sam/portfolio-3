0023b9e65ad7ebb549597382749b4e373eacf059
user <samuel_morgan_@outlook.com>
2023-09-20 18:34:57 -0300

    nav spacing

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <!-- Navbar.svelte -->
 <nav class="navbar absolute">
-	<ul class="flex items-center justify-center">
+	<ul class="flex items-center justify-center uppercase my-1">
 		<li class="mx-2"><a href="/">Home</a></li>
 		<li class="mx-2"><a href="/about">About</a></li>
 		<li class="mx-2"><a href="/projects">Projects</a></li>
