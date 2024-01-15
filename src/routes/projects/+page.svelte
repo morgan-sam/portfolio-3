18aa4e4237b370a8914d0af22d7a27d351d43839
user <samuel_morgan_@outlook.com>
2024-01-15 17:40:07 +0000

    grid on all pages

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,5 +3,11 @@
 	import Title from '$lib/components/Title.svelte';
 </script>
 
-<Title class="top-[27%]">Projects</Title>
-<Ball />
+<div class="grid grid-cols-3 items-center justify-center w-full h-full">
+	<div class="w-full h-full flex items-center justify-center">HELLO</div>
+	<div class="w-full h-full relative">
+		<Title class="top-[27%]">Projects</Title>
+		<Ball />
+	</div>
+	<div class="w-full h-full flex items-center justify-center">HELLO</div>
+</div>
