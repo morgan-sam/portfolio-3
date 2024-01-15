584438a23ceb8ea4cb3fea9dd3ba35f436df846d
user <samuel_morgan_@outlook.com>
2024-01-15 17:50:17 +0000

    border

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,12 @@
 <!-- Navbar.svelte -->
 <nav class="navbar">
 	<ul class="flex items-center justify-center uppercase text-4xl">
-		<li class="m-4 absolute top-0 left-0"><a href="/">Home</a></li>
-		<li class="m-4 absolute top-0 right-0"><a href="/resume">Resume</a></li>
-		<li class="m-4 absolute bottom-0 left-0"><a href="/projects">Projects</a></li>
-		<li class="m-4 absolute bottom-0 right-0"><a href="/contact">Contact</a></li>
+		<li class="absolute top-0 left-0"><a href="/">Home</a></li>
+		<li class="absolute top-0 right-0"><a href="/resume">Resume</a></li>
+		<li class="absolute bottom-0 left-0"><a href="/projects">Projects</a></li>
+		<li class="absolute bottom-0 right-0"><a href="/contact">Contact</a></li>
 	</ul>
+	<div class="grid-border" />
 </nav>
 
 <style>
@@ -23,5 +24,20 @@
 		font-style: normal;
 		letter-spacing: 10px;
 		color: #555;
+	}
+	.navbar ul li {
+		background: white;
+		padding: 15px;
+		margin: 10px;
+	}
+	.grid-border {
+		--border-width: 3%;
+		position: absolute;
+		top: var(--border-width);
+		left: var(--border-width);
+		width: calc(100% - var(--border-width) * 2);
+		height: calc(100% - var(--border-width) * 2);
+		border: 2px solid #555;
+		z-index: -1;
 	}
 </style>
