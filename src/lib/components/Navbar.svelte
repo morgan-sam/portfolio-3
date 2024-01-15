1113426474bdf2e457714ca100e7f15a39224fea
user <samuel_morgan_@outlook.com>
2024-01-15 17:11:39 +0000

    four corners

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <!-- Navbar.svelte -->
-<nav class="max-h-screen max-w-screen">
+<nav class="navbar max-h-screen max-w-screen">
 	<ul class="flex items-center justify-center uppercase text-4xl">
 		<li class="m-4 absolute top-0 left-0"><a href="/">Home</a></li>
 		<li class="m-4 absolute top-0 right-0"><a href="/resume">Resume</a></li>
@@ -7,3 +7,16 @@
 		<li class="m-4 absolute bottom-0 right-0"><a href="/contact">Contact</a></li>
 	</ul>
 </nav>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Calistoga&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000');
+
+	.navbar {
+		font-family: 'DM Sans', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 300;
+		font-style: normal;
+		letter-spacing: 10px;
+		color: #555;
+	}
+</style>
--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,3 +1,3 @@
-<h1 class="text-8xl z-10 absolute leading-[56px] {$$restProps.class || ''} ">
+<h1 class="text-8xl z-10 absolute leading-[56px] text-[#333333] {$$restProps.class || ''} ">
 	<slot />
 </h1>
