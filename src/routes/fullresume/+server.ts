3c295983e9409f9dd833edc29ffa0e5f124a9850
user <samuel_morgan_@outlook.com>
2024-01-30 10:06:57 -0500

    added full resume

--- src/routes/fullresume/+server.ts
+++ src/routes/fullresume/+server.ts
@@ -2,7 +2,7 @@ export async function GET() {
 	return new Response(null, {
 		status: 301,
 		headers: {
-			Location: 'https://docs.google.com/document/d/1pmVzgXfb-7HI-v5jELQV7r0TQPTy2AloCxadlO4fHKk'
+			Location: '/Samuel%20Morgan%20-%20Resume%202024.pdf'
 		}
 	});
 }
