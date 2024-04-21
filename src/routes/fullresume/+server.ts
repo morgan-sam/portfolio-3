803820f8a56874402db43856ed042a7002cda973
user <samuel_morgan_@outlook.com>
2024-04-21 15:37:09 +0100

    updated resume

--- src/routes/fullresume/+server.ts
+++ src/routes/fullresume/+server.ts
@@ -2,7 +2,7 @@ export async function GET() {
 	return new Response(null, {
 		status: 301,
 		headers: {
-			Location: '/Samuel%20Morgan%20-%20Resume%202024.pdf'
+			Location: '/Samuel%20Morgan%20Resume%202024.pdf'
 		}
 	});
 }
